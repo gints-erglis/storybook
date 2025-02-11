301aea72c723e36103381aee57c76cec0c5f2354
Gints Erglis <gints.erglis@wunder.io>
2025-02-11 22:42:19 +0200

    Add cards

--- src/stories/01-atoms/links/link-cta/link-cta-1/Link-cta-1.stories.ts
+++ src/stories/01-atoms/links/link-cta/link-cta-1/Link-cta-1.stories.ts
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
-
-import { LinkCta1 } from './Link-cta-1';
+import LinkCta1 from './Link-cta-1';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
