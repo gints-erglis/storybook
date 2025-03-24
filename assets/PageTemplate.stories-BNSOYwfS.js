b2ea3a1e72dc3fc58a375ebf00f44c67b719dc7a
Gints Erglis <gints.erglis@wunder.io>
2025-03-24 16:17:33 +0200

    Updates

---
+++ assets/PageTemplate.stories-BNSOYwfS.js
@@ -0,0 +1,5 @@
+import{j as a}from"./jsx-runtime-BjgbQsUx.js";import{P as o}from"./PageTemplate-C2CO3OMC.js";import"./index-D2MAbzvX.js";const c={title:"Templates/PageTemplate",component:o},e={args:{children:a.jsx("p",{children:"Content goes here..."})}};var r,t,s;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    children: <p>Content goes here...</p>
+  }
+}`,...(s=(t=e.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};const l=["Default"];export{e as Default,l as __namedExportsOrder,c as default};
