3329fe9af0102e691e5e2e97ab2d5a6f05a536c3
Gints Erglis <gints.erglis@wunder.io>
2025-03-24 13:47:50 +0200

    test how storybook-static folder impact deploy

---
+++ storybook-static/assets/Link-cta-1.stories-DEhCNWgF.js
@@ -0,0 +1,20 @@
+import{f as k}from"./index-DQLiMaGX.js";import{L as C}from"./Link-cta-1-xYYD-l1U.js";import"./jsx-runtime-BjgbQsUx.js";import"./index-D2MAbzvX.js";const S={title:"Atoms/Links/Link-CTA/Link-CTA-1",component:C,parameters:{layout:"centered",docs:{source:{type:"dynamic"}}},tags:["autodocs"],args:{onClick:k()}},r={args:{primary:!0,label:"Link CTA"}},a={args:{label:"Link CTA"}},e={args:{size:"large",label:"Link CTA"}},s={args:{size:"small",label:"Link CTA"}};var o,n,t;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    primary: true,
+    label: 'Link CTA'
+  }
+}`,...(t=(n=r.parameters)==null?void 0:n.docs)==null?void 0:t.source}}};var c,m,i;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    label: 'Link CTA'
+  }
+}`,...(i=(m=a.parameters)==null?void 0:m.docs)==null?void 0:i.source}}};var l,p,d;e.parameters={...e.parameters,docs:{...(l=e.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    size: 'large',
+    label: 'Link CTA'
+  }
+}`,...(d=(p=e.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};var g,u,L;s.parameters={...s.parameters,docs:{...(g=s.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    size: 'small',
+    label: 'Link CTA'
+  }
+}`,...(L=(u=s.parameters)==null?void 0:u.docs)==null?void 0:L.source}}};const f=["Primary","Secondary","Large","Small"];export{e as Large,r as Primary,a as Secondary,s as Small,f as __namedExportsOrder,S as default};
