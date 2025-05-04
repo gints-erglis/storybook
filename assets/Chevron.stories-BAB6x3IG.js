c5797b5cfce7d4e411a265777520f8e646efdec5
Gints Erglis <gints.erglis@wunder.io>
2025-05-04 14:20:04 +0300

    Updates

--- assets/Chevron.stories-BAB6x3IG.js
+++ assets/Chevron.stories-BAB6x3IG.js
@@ -1,4 +1,4 @@
-import{j as e}from"./jsx-runtime-BjgbQsUx.js";import{a4 as i,a5 as m}from"./index-D02HGfX7.js";import{C as c}from"./Chevron-BYRvdn4Y.js";import"./index-D2MAbzvX.js";import"./iframe-CwnmViXT.js";import"../sb-preview/runtime.js";import"./index-RigO-4kf.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-BIm0odtm.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";const p=`.chevron::before {
+import{j as e}from"./jsx-runtime-BjgbQsUx.js";import{a4 as i,a5 as m}from"./index-B6Xlxz_A.js";import{C as c}from"./Chevron-BYRvdn4Y.js";import"./index-D2MAbzvX.js";import"./iframe-BPl2iya4.js";import"../sb-preview/runtime.js";import"./index-RigO-4kf.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-BIm0odtm.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";const p=`.chevron::before {
   border-style: solid;
   border-width: 0.25em 0.25em 0 0;
   border-color: inherit;
